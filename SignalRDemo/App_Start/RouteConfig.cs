a33a8b5e21ed9ac5039c5aabeb1f3103e721644d
JSli-GitHub <muzi_li0323@163.com>
2019-08-20 15:21:53 +0800

    Initial commit

---
+++ SignalRDemo/App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+[assembly: OwinStartupAttribute(typeof(SignalRDemo.RouteConfig))]
+namespace SignalRDemo
+{
+    public class RouteConfig
+    {
+        /// <summary>
+        /// 配置你的信息 可以连带 [assembly: OwinStartupAttribute(typeof(SignalDemo.RouteConfig))]
+        /// 如果有Startup.cs可以写到 Startup 中
+        /// 或者 创建个 Startup.cs (我这里懒得创建 所以直接写到了路由配置上面 效果一样)
+        /// </summary>
+        /// <param name="app"></param>
+        #region MyRegion
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+        #endregion
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ SignalRDemo/ServerHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRDemo
+{
+    public class ServerHub : Hub
+    {
+        /// <summary>
+        /// 供客户端调用的服务器端代码
+        /// </summary>
+        /// <param name="message">客户端传来的消息</param>
+        public void Send(string message)
+        {
+            // 向所有客户端发送消息
+            Clients.All.sendMessage(message);
+        }
+    }
+}
